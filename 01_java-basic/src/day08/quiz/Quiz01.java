9a6943c76388eb2f1847d5fdd0963d37fd2bdebc
Leesangouk <8go4go@gmail.com>
2017-04-19 14:42:32 +0900

    without ReplaceAll

--- 01_java-basic/src/day08/quiz/Quiz01.java
+++ 01_java-basic/src/day08/quiz/Quiz01.java
@@ -2,13 +2,28 @@ package day08.quiz;
 
 public class Quiz01 {
 	
+//	public String remove(String data, char delch) {
+//		
+//		String returnStr = data.replaceAll("[o]", "");
+//		return returnStr;
+//	}
+//			
 	public String remove(String data, char delch) {
+		
+		StringBuffer sb = new StringBuffer();
+		
+		for(int i=0; i < data.length(); i++) {
+			if(delch == data.charAt(i)) {
+				continue;
+			} else {
+				sb.append(data.charAt(i));
+			}
+		}
 		
 		String returnStr = data.replaceAll("[o]", "");
 		return returnStr;
 	}
 			
-	
 	
 	public static void main(String[] args) {
 		Quiz01 q = new Quiz01();
