98606fe526ac1f2ed0ad07d6ff18307333c34ef3
Leesangouk <8go4go@gmail.com>
2017-05-10 20:44:38 +0900

    File Upload를 위한 FileVO 추가

---
+++ h03_springWeb/src/main/java/kr/heartof/springWeb_01/vo/user/AuctionFileVO.java
@@ -0,0 +1,57 @@
+package kr.heartof.springWeb_01.vo.user;
+
+public class AuctionFileVO {
+	private Integer FILE_NO;
+	private String FILE_NM;
+	private String FILE_PATH;
+	private Integer REG_NO;
+	
+	/**
+	 * @return the fILE_NO
+	 */
+	public Integer getFILE_NO() {
+		return FILE_NO;
+	}
+	/**
+	 * @param fILE_NO the fILE_NO to set
+	 */
+	public void setFILE_NO(Integer fILE_NO) {
+		FILE_NO = fILE_NO;
+	}
+	/**
+	 * @return the fILE_NM
+	 */
+	public String getFILE_NM() {
+		return FILE_NM;
+	}
+	/**
+	 * @param fILE_NM the fILE_NM to set
+	 */
+	public void setFILE_NM(String fILE_NM) {
+		FILE_NM = fILE_NM;
+	}
+	/**
+	 * @return the fILE_PATH
+	 */
+	public String getFILE_PATH() {
+		return FILE_PATH;
+	}
+	/**
+	 * @param fILE_PATH the fILE_PATH to set
+	 */
+	public void setFILE_PATH(String fILE_PATH) {
+		FILE_PATH = fILE_PATH;
+	}
+	/**
+	 * @return the rEG_NO
+	 */
+	public Integer getREG_NO() {
+		return REG_NO;
+	}
+	/**
+	 * @param rEG_NO the rEG_NO to set
+	 */
+	public void setREG_NO(Integer rEG_NO) {
+		REG_NO = rEG_NO;
+	}
+}
---
+++ h03_springWeb/src/main/java/kr/heartof/springWeb_01/vo/user/USRFileVO.java
@@ -0,0 +1,56 @@
+package kr.heartof.springWeb_01.vo.user;
+
+public class USRFileVO {
+	private Integer FILE_NO;
+	private String FILE_NM;
+	private String FILE_PATH;
+	private Integer USR_NO;
+	/**
+	 * @return the fILE_NO
+	 */
+	public Integer getFILE_NO() {
+		return FILE_NO;
+	}
+	/**
+	 * @param fILE_NO the fILE_NO to set
+	 */
+	public void setFILE_NO(Integer fILE_NO) {
+		FILE_NO = fILE_NO;
+	}
+	/**
+	 * @return the fILE_NM
+	 */
+	public String getFILE_NM() {
+		return FILE_NM;
+	}
+	/**
+	 * @param fILE_NM the fILE_NM to set
+	 */
+	public void setFILE_NM(String fILE_NM) {
+		FILE_NM = fILE_NM;
+	}
+	/**
+	 * @return the fILE_PATH
+	 */
+	public String getFILE_PATH() {
+		return FILE_PATH;
+	}
+	/**
+	 * @param fILE_PATH the fILE_PATH to set
+	 */
+	public void setFILE_PATH(String fILE_PATH) {
+		FILE_PATH = fILE_PATH;
+	}
+	/**
+	 * @return the uSR_NO
+	 */
+	public Integer getUSR_NO() {
+		return USR_NO;
+	}
+	/**
+	 * @param uSR_NO the uSR_NO to set
+	 */
+	public void setUSR_NO(Integer uSR_NO) {
+		USR_NO = uSR_NO;
+	}
+}
